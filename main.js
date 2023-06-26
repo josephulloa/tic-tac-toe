c5428157dc35e03d335d12993d7bf25dab349a71
Joseph <josephulloa1120@hotmail.com>
2023-06-26 15:19:45 -0600

    funcion de agregar X

---
+++ main.js
@@ -0,0 +1,18 @@
+let arriba2 = document.getElementById("boxA2");
+let medio2 = document.getElementById("boxM2");
+let abajo2 = document.getElementById("boxB2");
+
+function arriba() {
+  arriba2.innerHTML = "X";
+}
+
+
+function medio() {
+  medio2.innerHTML = "X";
+}
+
+
+function abajo() {
+    abajo2.innerHTML = "X";
+}
+
