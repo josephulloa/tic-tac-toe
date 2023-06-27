043b0ddb721d6cf345020230a0d2af14d3a506af
Joseph <josephulloa1120@hotmail.com>
2023-06-27 16:11:13 -0600

    funcionalidad del juego

--- main.js
+++ main.js
@@ -1,18 +1,68 @@
-let arriba2 = document.getElementById("boxA2");
-let medio2 = document.getElementById("boxM2");
-let abajo2 = document.getElementById("boxB2");
+let jugador1 = true;
+let caja = document.getElementsByClassName("box");
 
-function arriba() {
-  arriba2.innerHTML = "X";
+let aleatorio = Math.floor(Math.random() * 9);
+
+for (let i = 0; i < caja.length; i++) {
+  caja[i].addEventListener("click", userMove);
+}
+
+function userMove(e) {
+  let cajaValue = e.target.innerHTML;
+  if (!cajaValue.length) {
+    e.target.innerHTML = jugador1 = "X";
+    //  'O';jugador1 =!jugador1;
+  }
+
+  validar(caja);
 }
 
 
-function medio() {
-  medio2.innerHTML = "X";
+function validar(caja) {
+  for (let i = 0; i < 9; i++) {
+    let valor = caja[i].textContent;
+    if (valor == "") {
+      caja[aleatorio].innerHTML = "O";
+    }
+  }
+}
+
+// const computadoraMove= {
+//   if (valor="") {
+//   do{ elegir= Math.floor(Math.random() * 9);
+//   } while ([elegir])!="" {
+    
+//   }
+//  }
+  
+  
+ 
 }
 
 
-function abajo() {
-    abajo2.innerHTML = "X";
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function validar() {
+//   let validar = ingresar.value;
+//   if (validar.length == 0) {
+
+//   }
+
+// }
+
+// function computadora(jugador1= !jugador1) {
+
+//   setTimeout.aleatorio(computadora, 1500);
+// }
